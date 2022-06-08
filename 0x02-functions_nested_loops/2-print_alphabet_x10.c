23d0deaba33727e8a0d49c66719a68f003ac0096
Chuksdgreat <chuksdgreat1018@gmail.com>
2022-06-07 20:50:31 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 -> prints the lowercase alphabets
+*
+* Return: Always 0.
+*/
+
+void print_alphabet_10x(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'Z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n')
+	return (0);
+	}
+}
+
