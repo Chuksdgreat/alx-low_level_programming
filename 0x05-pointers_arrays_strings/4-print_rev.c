8f18eeb74fe4ad94911be498d7b36aee62af2f5f
Chuksdgreat <chuksdgreat1018@gmail.com>
2022-06-13 16:49:56 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#inlude "main.h"
+
+/**
+ * print_rev -> printing a strin in reverse
+ * @s: the string to be printed in rev
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\n')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+}
