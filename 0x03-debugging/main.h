30f7c18aa237a3eb1cd72b23aebe3921dc58d73d
Chuksdgreat <chuksdgreat1018@gmail.com>
2022-06-13 02:44:32 -0700

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,10 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaning_days(int, int, int);
-int convert_day(int month, int day);
-void print_most_numbers(void);
-void more numbers(void)
 
 #endif /* MAIN_H */
